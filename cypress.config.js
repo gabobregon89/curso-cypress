4340498786dcd2e76b842c478b637220dd84b0ad
gabobregon89 <gab.obregon89@gmail.com>
2025-08-19 19:01:37 -0300

    Commit inicial de creacion de proyecto

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin(on, config);
+      on("file:preprocessor", createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      }));
+      return config;
+    },
+    specPattern: "cypress/e2e/**/*.feature",
+  },
+});
