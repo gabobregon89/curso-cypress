bef76699bd4dd0a9e036ea24b6f1b0150a6d7c4f
Gabriel Obregon <gab.obregon89@gmail.com>
2025-08-31 15:58:01 -0300

    selucion de ejercicio clase 3

---
+++ cypress/support/step_definitions/clase3.js
@@ -0,0 +1,36 @@
+import { Given, When } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("el usuario abre la web de automation exercise", function () {
+    cy.visit("https://automationexercise.com/");
+});
+
+When("el usuario hace clic en el boton products", function () {
+    cy.get('.shop-menu > .nav > :nth-child(2) > a').click();
+});
+
+When("el usuario realiza la busqueda del producto {string}", function (producto) {
+    cy.get('#search_product').type(producto);
+    cy.get('#submit_search').click();
+});
+
+When("el usuario obtiene el precio del producto", function () {
+    cy.get('.productinfo > h2').then(function (element) {
+        this.textoPrecio = element.text();
+        cy.log(this.textoPrecio);
+    });
+});
+
+When("el usuario hace clic en en el boton view product", function () {
+    cy.get('.choose > .nav > li > a').click();
+});
+
+Then("el sistema muestra la informacion de {string}", function (element) {
+    cy.get('.product-information > h2').should('have.text', element);
+});
+
+When("el sistema muestra el mismo precio del producto que se obtuvo en la busqueda", function () {
+    cy.get('.product-information > span').then(function (element) {
+        const textoPrecioDetalle = element.text();
+        expect(textoPrecioDetalle).to.equal(this.textoPrecio);
+    });
+});
